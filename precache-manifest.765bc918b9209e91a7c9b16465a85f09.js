7f725c3fb8d126fd17c2368f78ff11c2ef7523e0
nicky <wormvirus94@gmail.com>
2019-03-11 05:15:09 +0100

    Updates

---
+++ precache-manifest.765bc918b9209e91a7c9b16465a85f09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1335e0acc303ca712354",
+    "url": "/safebox/static/js/runtime~main.1f7b49be.js"
+  },
+  {
+    "revision": "a8441ab0bda76adf4f47",
+    "url": "/safebox/static/js/main.4eb31659.chunk.js"
+  },
+  {
+    "revision": "5d38434d1bbecf474fa5",
+    "url": "/safebox/static/js/2.eb7024da.chunk.js"
+  },
+  {
+    "revision": "a8441ab0bda76adf4f47",
+    "url": "/safebox/static/css/main.e94ea30e.chunk.css"
+  },
+  {
+    "revision": "ab5d477667ef732250894140717371a4",
+    "url": "/safebox/index.html"
+  }
+];
